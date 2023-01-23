663be749f85f00a0869895d152724ff144608f82
Halil İbrahim Dursun <48247076+hymlife@users.noreply.github.com>
2023-01-23 15:18:19 +0300

    quiz üzerinde değişiklik

--- screens/quiz.js
+++ screens/quiz.js
@@ -2,40 +2,91 @@ import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import {resolve} from 'path';
 
+const shuffleArray = array => {
+  for (let i = array.lenght - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+};
+
 const Quiz = ({navigation}) => {
   const [questions, setQuestions] = useState();
   const [ques, setQues] = useState(0);
+  const [options, setOptions] = useState([]);
+  const [score, setScore] = useState(0);
+
   const getQuiz = async () => {
-    const url = 'https://opentdb.com/api.php?amount=10&type=multiple';
+    const url =
+      'https://opentdb.com/api.php?amount=10&type=multiple&encode=url3986';
     const res = await fetch(url);
     const data = await res.json();
     setQuestions(data.results);
+    setOptions(generateOptionsAndShuffle(data.results[0]));
   };
   useEffect(() => {
     getQuiz();
   }, []);
 
+  const handleNextPress = () => {
+    setQues(ques + 1);
+    setOptions(generateOptionsAndShuffle(questions[ques + 1]));
+  };
+
+  const generateOptionsAndShuffle = _question => {
+    const options = [..._question.incorrect_answers];
+    options.push(_question.corrent_answer);
+    shuffleArray(options);
+
+    return options;
+  };
+
+  const handleSelectedOption = _option => {
+    if (_option === questions[ques].corrent_answer) {
+      setScore(score + 10);
+    }
+    setQues(ques + 1);
+    setOptions(generateOptionsAndShuffle(questions[ques + 1]));
+    console.log(_option === questions[ques].corrent_answer);
+  };
   return (
     <View style={styles.container}>
       {questions && (
         <View style={styles.parent}>
           <View style={styles.top}>
-            <Text style={styles.question}>Sa Sa Sa</Text>
+            <Text style={styles.question}>
+              {decodeURIComponent(questions[ques].question)}
+            </Text>
           </View>
 
           <View style={styles.options}>
-            <TouchableOpacity style={styles.optionButton}>
-              <Text style={styles.option}>Cool Question</Text>
+            <TouchableOpacity
+              style={styles.optionButton}
+              onPress={() => handleSelectedOption(options[0])}>
+              <Text style={styles.option}>
+                {decodeURIComponent(options[0])}
+              </Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.optionButton}>
-              <Text style={styles.option}>Cool Question</Text>
+            <TouchableOpacity
+              style={styles.optionButton}
+              onPress={() => handleSelectedOption(options[1])}>
+              <Text style={styles.option}>
+                {decodeURIComponent(options[1])}
+              </Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.optionButton}>
-              <Text style={styles.option}>Cool Question</Text>
+            <TouchableOpacity
+              style={styles.optionButton}
+              onPress={() => handleSelectedOption(options[2])}>
+              <Text style={styles.option}>
+                {decodeURIComponent(options[2])}
+              </Text>
             </TouchableOpacity>
 
-            <TouchableOpacity style={styles.optionButton}>
-              <Text style={styles.option}>Cool Question</Text>
+            <TouchableOpacity
+              style={styles.optionButton}
+              onPress={() => handleSelectedOption(options[3])}>
+              <Text style={styles.option}>
+                {decodeURIComponent(options[3])}
+              </Text>
             </TouchableOpacity>
           </View>
 
@@ -44,9 +95,17 @@ const Quiz = ({navigation}) => {
               <Text style={styles.buttonText}>SKİP</Text>
             </TouchableOpacity>
 
-            <TouchableOpacity style={styles.button}>
-              <Text style={styles.buttonText}>NEXT</Text>
-            </TouchableOpacity>
+            {ques !== 9 && (
+              <TouchableOpacity style={styles.button} onPress={handleNextPress}>
+                <Text style={styles.buttonText}>NEXT</Text>
+              </TouchableOpacity>
+            )}
+
+            {ques === 9 && (
+              <TouchableOpacity style={styles.button} onPress={() => null}>
+                <Text style={styles.buttonText}>SHOW RESULT</Text>
+              </TouchableOpacity>
+            )}
           </View>
         </View>
       )}
